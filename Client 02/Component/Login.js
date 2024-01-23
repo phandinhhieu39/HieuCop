fc867e87978738e87c29cdddc42aba49f72c7876
phandinhhieu39 <144341752+phandinhhieu39@users.noreply.github.com>
2024-01-23 17:49:42 +0700

    nộp bài báo cáo
    Phan Đình Hiếu_2121110296

--- Client 02/Component/Login.js
+++ Client 02/Component/Login.js
@@ -1,115 +1,119 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet, Text, View, TextInput, TouchableOpacity,
+  KeyboardAvoidingView, ImageBackground
+} from 'react-native';
+import React, { useState } from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Login = () => {
-    const navigation = useNavigation();
-
-    const handleLoginPress = () => {
-    // Chuyển đến trang "Home"
-    navigation.navigate('Home');
+function Login({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async () => {
+    const accounts = await AsyncStorage.getItem("user");
+    if (accounts) {
+      const accountArray = JSON.parse(accounts);
+      var flag = accountArray.find((account) => 
+      account.username == username && account.password == password
+  );
+      if(flag){
+        alert("Đăng nhập thành công");
+        navigation.navigate("Home");
+      }
+      else{
+        alert("Email hoặc mật khẩu không chính xác");
+        return;
+      }
+    }
   };
-    return (
-        <View style={styles.container}>
-            <View style={styles.content}>
-                <Image
-                    source={require('../assets/login.png')}
-                    style={styles.image}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="User Name"
-                    keyboardType="input your email-address"
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="Password"
-                    secureTextEntry
-                />
-                <TouchableOpacity style={styles.button} onPress={handleLoginPress}>
-                    <Text style={styles.buttonText}>Login</Text>
-                </TouchableOpacity>
-               
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}
+    >
+      <View>
+        <ImageBackground style={styles.background} >
+          <Text style={styles.title}>ĐĂNG NHẬP</Text>
+          <View style={{ marginTop: 40 }}>
+            <View style={styles.iconinput}>
+              <Icon name="user" size={30} color="gray" />
+              <TextInput style={styles.input} placeholder=" Nhập tên đăng nhập hoặc email" onChangeText={(e) => setUsername(e)}/>
             </View>
-        </View>
-    );
-};
+
+            <View style={styles.iconinput}>
+              <Icon name="lock" size={30} color="gray" />
+              <TextInput style={styles.input} placeholder=" Nhập mật khẩu"  onChangeText={(e) => setPassword(e)}/>
+            </View>
+            <Text style={{ alignSelf: 'flex-end' }}>Quên mật khẩu?</Text>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={() => handleLogin()}>
+            <Text style={styles.buttonText}>Đăng nhập</Text>
+          </TouchableOpacity>
+          <View style={styles.rowContainer}>
+            <Text style={{ alignSelf: 'flex-end' }}>Bạn chưa có tài khoản? </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+              <Text style={{ textAlign: 'center', color: 'red' }}> Đăng kí</Text>
+            </TouchableOpacity>
+          </View>
+          <StatusBar style="auto" />
+        </ImageBackground>
+      </View>
+    </KeyboardAvoidingView>
+
+
+  );
+}
 
 const styles = StyleSheet.create({
-    input: {
-        color: 'gray',
-        backgroundColor: '#ffff',
-        margin:10,
-        borderRadius: 20,
-        height: 70,
-        width: '70%',
-        borderWidth: 1,
-        marginBottom: 10,
-        paddingLeft: 10,
-    },
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-       
-    },
-    buttonUserName:{
-justifyContent:'left',
-    },
-    content: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    image: {
-        width: 200,
-        height: 200,
-        marginBottom: 16,
-    },
-    text: {
-        fontSize: 18,
-        marginBottom: 16,
-    },
-    button: {
-        backgroundColor: 'black',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 10,
-        width: '70%',
-        marginTop:30,
-        alignItems: 'center',
-    },
-    buttonSignInGoole: {
-        backgroundColor: 'white',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 10,
-        width: '70%',
-        marginTop:30,
-        alignItems: 'center',
-    },
-    buttonSignInFaceBook: {
-        backgroundColor: '#007bff',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 10,
-        width: '70%',
-        marginTop:30,
-        alignItems: 'center',
-    },
-    buttonText: {
-        color: '#DDDDDD',
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    footer: {
-        padding: 16,
-        backgroundColor: '#f9f9f9',
-        alignItems: 'center',
-    },
-    footerText: {
-        fontSize: 12,
-        color: '#888',
-    },
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  iconinput: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomColor: 'black',
+    borderBottomWidth: 0.3,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    width: 300,
+    paddingHorizontal: 10,
+
+  },
+  button: {
+    backgroundColor: 'black',
+    padding: 10,
+    marginTop: 30,
+    borderRadius: 20
+  },
+  buttonText: {
+    width: 300,
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 420
+  },
 });
 
 export default Login;
--- Client 02/Component/Register.js
+++ Client 02/Component/Register.js
@@ -1,114 +1,143 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity,
+     KeyboardAvoidingView, ImageBackground } from 'react-native';
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const RegisterPage = () => {
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigation = useNavigation();
+function Register({ navigation }) {
 
-  const handleRegister = () => {
-    // Xử lý việc đăng ký
-    console.log('Đăng ký thành công!');
-    navigation.navigate('RegisterSuccess');
-  };
+  const HandleRegisterCheck = async () => {
 
-  const handleLoginPress = () => {
-    navigation.navigate('Login');
-  }
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Đăng ký</Text>
-
-      <TextInput
-        style={styles.input}
-        placeholder="Họ và tên"
-        value={fullName}
-        onChangeText={(text) => setFullName(text)}
-      />
-
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-        keyboardType="email-address"
-      />
-
-      <TextInput
-        style={styles.input}
-        placeholder="Mật khẩu"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        secureTextEntry
-      />
-
-      <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
-        <Text style={styles.registerButtonText}>Đăng ký</Text>
-      </TouchableOpacity>
-
-      <View style={styles.footer}>
-        <Text style={styles.footerText}>Đã có tài khoản?</Text>
-        <TouchableOpacity>
-          <Text style={styles.loginLink} onPress={handleLoginPress}>Đăng nhập ngay</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    const newUser = {
+      username: username,
+      password: password,
+    };
+    const existingAccount = await AsyncStorage.getItem("user");
+    if (existingAccount) {
+      const parsedAccount = JSON.parse(existingAccount);
+      var flag = parsedAccount.find((account) => 
+      account.username == username
   );
-};
+  if(flag){
+    alert("Tài khoản đã tồn tại");
+    return;
+  }
+      parsedAccount.push(newUser);
+      AsyncStorage.setItem("user", JSON.stringify(parsedAccount)).then(() => {
+        AsyncStorage.getItem("user").then((res) => {
+          alert("Đăng kí thành công");
+          navigation.navigate("Login");
+        });
+      });
+    } else {
+      AsyncStorage.setItem("user", JSON.stringify([newUser])).then(() => {
+        AsyncStorage.getItem("user").then((res) => {
+          alert("Đăng kí thành công");
+          navigation.navigate("Login");
+        });
+      });
+    }
+  };
+const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [repassword, setRePassword] = useState("");
+  return (
+    <KeyboardAvoidingView
+  behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+  style={styles.container}
+>
+  <View>
+  <ImageBackground style={styles.background} >
+        <Text style={styles.title}>ĐĂNG KÍ</Text>
+        <View style={{marginTop:40}}>
+          <View style={styles.iconinput}>
+            <Icon name="user" size={30} color="gray" />
+            <TextInput style={styles.input} placeholder=" Nhập tên đăng nhập hoặc email" onChangeText={(e) => setUsername(e)}/>
+          </View>
+         
+          <View style={styles.iconinput}>
+            <Icon name="lock" size={30} color="gray" />
+            <TextInput style={styles.input} placeholder=" Nhập mật khẩu" onChangeText={(e) => setPassword(e)}/>
+          </View>
+          <View style={styles.iconinput}>
+            <Icon name="lock" size={30} color="gray" />
+            <TextInput style={styles.input} placeholder=" Xác nhận mật khẩu" onChangeText={(e) => setRePassword(e)}/>
+          </View>
+         
+        </View>
+        <TouchableOpacity style={styles.button} onPress={() => HandleRegisterCheck()}>
+          <Text style={styles.buttonText}>Đăng kí</Text>
+        </TouchableOpacity>
+        <View style={styles.rowContainer}>
+          <Text style={{ alignSelf: 'flex-end' }}>Bạn đã có tài khoản? </Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+            <Text style={{ textAlign: 'center', color: 'red' }}> Đăng nhập</Text>
+          </TouchableOpacity>
+        </View>
+        <StatusBar style="auto" />
+      </ImageBackground>
+  </View>
+</KeyboardAvoidingView>
+
+  
+  );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 20,
-    backgroundColor: '#fff',
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  iconinput:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomColor:'black',
+    borderBottomWidth:0.3,
+    marginBottom: 10,
   },
   title: {
-    fontSize: 24,
+    fontSize: 30,
+    color:'black',
     fontWeight: 'bold',
-    marginBottom: 20,
-    textAlign: 'center',
-    
-    
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   input: {
-    height: 48,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 4,
+    height: 40,
+    width: 300,
+  
+   
+    
     paddingHorizontal: 10,
-    marginBottom: 12,
+   
   },
-  registerButton: {
-    height: 48,
+  button: {
     backgroundColor: 'black',
-    borderRadius: 4,
+    padding: 10,
+    marginTop: 30,
+    borderRadius:20
+
+  },
+  buttonText: {
+    width: 300,
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  background: {
+    flex: 1,
+
+    backgroundColor: 'white',
+    resizeMode: 'cover',
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 12,
-  },
-  registerButtonText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  footer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  footerText: {
-    fontSize: 14,
-    marginRight: 4,
-  },
-  loginLink: {
-    fontSize: 14,
-    color: 'black',
-    fontWeight: 'bold',
+    width:420
   },
 });
 
-export default RegisterPage;
+export default Register;
